40acc1ead066f034258fb155b6be81c1007cfcd3
zhengxun890311 <zhengxun890311@gmail.com>
2019-03-14 14:11:26 -0700

    Revert "modularization demo"
    This reverts commit 6f74453059331848ea870dd7b2727b1887d6e09e.

--- server/config/routes.js
+++
@@ -1,9 +0,0 @@
-let UsersCtrl=require('./../controllers/UsersCtrl.js')
-
-module.exports=function(app){
-app.get('/',UsersCtrl.index);
-app.post('/add_user',UsersCtrl.create);
-app.post('/mongooses/destroy/:id',UsersCtrl.destroy);
-app.post('/mongooses/show_update/:id',UsersCtrl.show_update);
-app.post('/mongooses/edit/:id',UsersCtrl.update);
-}
--- server/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-
-var userSchema=new mongoose.Schema({
-    name:{type:String,required:true,minlength:2},
-    quote:{type:String,required:true,minlength:5}
-},{timestamps:true});
-
-const users = mongoose.model('users',userSchema);
